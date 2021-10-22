27de71d4579142eb3573e64d7b1ed623937c653f
ybong <>
2021-10-22 01:46:47 +0000

    mutex_test

--- test.c
+++ test.c
@@ -1,13 +1,16 @@
 # include "philo.h"
 
-void *thread_func(void *ptr)
-{
-	int count = 0;
+int g_count = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-	for(int i=0; i < 10000; i++)
-		count++;
-	printf("%s\n", ptr);
-	return (0);
+void *thread_func(void *str)
+{
+	pthread_mutex_lock(&mutex);
+	printf("%s start\n", (char*)str);
+	for(int i=0; i < 10000000; i++)
+		g_count++;
+	printf("%s: %d\n", (char*)str, g_count);
+	pthread_mutex_unlock(&mutex);
 }
 
 
@@ -16,15 +19,14 @@ int main (void)
 	char *msg1="Thread1";
 	char *msg2="Thread2";
 
-	pthread_t thread1;
-	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+	pthread_t thread1, thread2;
+	int status;
 	
-	pthread_mutex_lock(&mutex);
-
-	pthread_create(&thread1, NULL, thread_func, msg1);
-	pthread_join(thread1, NULL);
+	pthread_create(&thread1, NULL, thread_func, (void*)msg1);
+	pthread_create(&thread2, NULL, thread_func, (void*)msg2);
+	pthread_join(thread1, (void*)&status);
+	pthread_join(thread2, (void*)&status);
 	printf("this is main.\n");
 	printf("fin_main\n");
-	while(1){}
 	return(0);
 }
