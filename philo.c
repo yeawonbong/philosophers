b7636484e417722c51dba398ee272504ae69d979
yeawonbong <ybong.develop@gmail.com>
2021-11-17 17:30:20 +0900

    1117_fin_norm

--- philo.c
+++ philo.c
@@ -43,7 +43,7 @@ static void	free_all(t_philo *philo)
 		pthread_mutex_destroy(&philo->forks[i]);
 		i++;
 	}
-	// pthread_mutex_destroy(&philo->print_lock);
+	pthread_mutex_destroy(&philo->print_lock);
 	pthread_mutex_destroy(&philo->exit);
 	free(philo->parr);
 }
